2efc611c37276684428f4503a5a84257f75221de
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2025-03-14 00:18:14 -0300

    Criando projeto do consumidor.

---
+++ frequencia.consumidor/src/main/java/com/br/frequencia/consumidor/Application.java
@@ -0,0 +1,20 @@
+package com.br.frequencia.consumidor;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/horario-ponto";
+	}
+}
